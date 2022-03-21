0fed7e93587cf789aa6f99613dcb6996d17823e3
Bredon <arileo.pereira@gmail.com>
2022-03-21 11:32:31 +0100

    firstmodal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import Modal from 'react-modal';
 import './App.css';
+import React, { useState } from 'react'
 
+Modal.setAppElement('#root')
 function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setModalIsOpen(true)}
+      style={
+        {
+          width: '180px',
+          marginTop: '15rem',
+        }
+      }>Cliquez ici pour ouvrir</button>
+
+      {/* here we can close the modal by pressing the overlay*/}
+      <Modal isOpen={modalIsOpen} 
+       onRequestClose={() => setModalIsOpen(false)}
+       style={
+         {
+           overlay: {
+             backgroundColor: '#c5c5c5'
+           },
+           content: {
+             background:'white'},
+         }
+       }>
+        <h2>Ceci est une Modal avec React</h2>
+        <p>Génial !!!</p>
+        <div>
+          <button onClick={() => setModalIsOpen(false)}>Cliquez ici pour fermer</button>
+        </div>
+      </Modal>
+
     </div>
   );
 }
